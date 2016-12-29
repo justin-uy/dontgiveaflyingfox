802d55bb5a582f1b3caba0650cc34dcc28e17641
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 00:33:58 -0800

    fixed nginx config

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    res.send("hello, world!")
+    res.send("welcome to justintrigue.us! Site is under construction and is we be finished some day.");
   });
   // 404
   app.get('*', (req, res) => {
