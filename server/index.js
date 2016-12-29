23ed3658a1b0ef7af2e3b089b4241b3edfc32b12
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 15:50:25 -0800

    setup view rendering and fix some redirects

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 require('./hostRouter')(app);
 require('node-jsx').install();
 const port = 3000;
+
+// set up ejs for templating. You can use whatever
+app.set('view engine', 'ejs');
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
--- server/sites/dontgiveaflyingfox.com/www/controllers/index.js
+++ server/sites/dontgiveaflyingfox.com/www/controllers/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   init: (req, res) => {
-    res.send('Welcome to www.dontgiveaflyingfox.com! Site is under construction and will be finished some day.');
+    res.render('base.ejs');
   }
 };
