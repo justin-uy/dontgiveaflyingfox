3756e932ef02071e7657aa4bf388a9cb5696aa74
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 00:49:36 -0800

    fix stupidness

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
   app.get('/', (req, res) => {
-    res.send("welcome to justintrigue.us! Site is under construction and is we be finished some day.");
+    res.send("Welcome to justintrigue.us! Site is under construction and will be finished some day.");
   });
   // 404
   app.get('*', (req, res) => {
