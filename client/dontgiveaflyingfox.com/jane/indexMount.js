daa6f34afa6b016c406f98813f9ea31e36edcd19
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 23:49:16 -0800

    update jane site to mount correct component

--- client/dontgiveaflyingfox.com/jane/indexMount.js
+++ client/dontgiveaflyingfox.com/jane/indexMount.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-const HomePage = require('../../../shared/components/justin/HomePage.react');
+const HomePage = require('../../../shared/components/jane/HomePage.react');
 
 ReactDOM.render(
   <HomePage />,
