878087ceba2536b9b2d25593a1d93ee59589463a
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 21:45:55 -0800

    evaluate javascript expression instead of rendering string literal

--- shared/components/HeaderRight.react.js
+++ shared/components/HeaderRight.react.js
@@ -3,7 +3,7 @@ const React = require('react');
 module.exports = (props) => {
   return (
     <ul className="header-right">
-      {props.links.map((v, i) => <li key={'item-' + i}><a href={v.href}>v.label</a></li>)}
+      {props.links.map((v, i) => <li key={'item-' + i}><a href={v.href}>{v.label}</a></li>)}
     </ul>
   );
 };
