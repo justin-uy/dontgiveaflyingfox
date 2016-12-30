bea99482296a3118370e2ad20b3e36780a9a2fa7
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 21:55:27 -0800

    fix the gallery

--- shared/components/ImageGallery.react.js
+++ shared/components/ImageGallery.react.js
@@ -1,14 +1,20 @@
 const React = require('react');
 
 module.exports = (props) => {
+  let images = [];
+
+  for (var i = 0; i < props.count; i++) {
+    images.push(
+      <div key={'gallery-item-' + i} className="image-container">
+        <div className="image" />
+        <span>image {i}</span>
+      </div>
+    );
+  }
+
   return (
     <div className="image-gallery">
-      (new Array(props.count)).map((_v, i) =>
-        <div key={'gallery-item-' + i} className="image-container">
-          <div className="image" />
-          <span>image {i}</span>
-        </div>
-      )}
+      {images}
     </div>
   );
 };
