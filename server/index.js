faa2e7d71d1ed62a0bb4d1374e1b15d1b6946e8f
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 17:10:57 -0800

    Render react on the server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+require('runtime-transpiler').start({
+  js: {
+    jsx: true
+  }
+});
 require('./hostRouter')(app);
-require('node-jsx').install();
 const port = 3000;
 
 // set up ejs for templating. You can use whatever
--- server/sites/dontgiveaflyingfox.com/www/controllers/index.js
+++ server/sites/dontgiveaflyingfox.com/www/controllers/index.js
@@ -1,5 +1,12 @@
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const ViewContainer = require('../views/index/ViewContainer.react');
+
 module.exports = {
   init: (req, res) => {
-    res.render('base.ejs');
+    res.render('base.ejs', {
+      title: 'dontgiveaflyingfox',
+      pageContent: ReactDOMServer.renderToString(<ViewContainer />)
+    });
   }
 };
