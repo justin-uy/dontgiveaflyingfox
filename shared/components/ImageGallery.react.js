150b67e6c73c9fa2ff72c8d900f1d27c36441cb7
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 21:50:32 -0800

    fix styling and image gallery

--- shared/components/ImageGallery.react.js
+++ shared/components/ImageGallery.react.js
@@ -3,7 +3,7 @@ const React = require('react');
 module.exports = (props) => {
   return (
     <div className="image-gallery">
-      {[].fill(null, 0, props.count).map((_v, i) =>
+      (new Array(props.count)).map((_v, i) =>
         <div key={'gallery-item-' + i} className="image-container">
           <div className="image" />
           <span>image {i}</span>
