6443660f73fad50a22703a474834239e97f90eee
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 19:34:57 -0800

    less trivial isomorphic example

--- shared/components/ViewContainer.react.js
+++ shared/components/ViewContainer.react.js
@@ -1,7 +1,28 @@
 const React = require('react');
 
-module.exports = (props) => {
-  return (
-    <div>This is from react</div>
-  );
+module.exports = class ViewContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+
+    this._incrementCount = this._incrementCount.bind(this);
+  }
+
+  _incrementCount() {
+    this.setState({
+      count: this.state.count++
+    });
+  }
+
+  render() {
+    return (
+      <form name="test">
+        <p>This is from react. The button has been clicked: {this.state.count} times</p>
+        <br />
+        <button type="submit" onClick={this._incrementCount}>Click Me!</button>
+      </form>
+    );
+  }
 };
