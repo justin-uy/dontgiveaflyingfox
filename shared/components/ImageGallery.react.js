e98d53cd89343a6898543106d73fc26f658b200b
Justin Uy <justin.uy@user.noreply.github.com>
2016-12-29 22:13:13 -0800

    make image gallery item selectable

--- shared/components/ImageGallery.react.js
+++ shared/components/ImageGallery.react.js
@@ -1,21 +1,56 @@
 const React = require('react');
 
-module.exports = (props) => {
-  let images = [];
+module.exports = class ImageGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
 
-  for (var i = 0; i < props.count; i++) {
-    images.push(
-      <div key={'gallery-item-' + i} className="image-container">
-        <div className="image" />
-        <span>image {i}</span>
+  _getOnImageClickCallback(i) {
+    return () => {
+      if (this.state.selected !== i) {
+        this.setState({
+          selected: i
+        });
+      } else {
+        this.setState({
+          selected: null
+        });
+      }
+    };
+  }
+
+  render() {
+    let images = [];
+    const {selected} = this.state;
+
+    for (var i = 0; i < this.props.count; i++) {
+      images.push(
+        <div
+          key={'gallery-item-' + i}
+          className="image-container"
+          onClick={this._getOnImageClickCallback(i)}>
+          <div className="image" />
+          <span>image {i}</span>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        {
+          selected !== null &&
+          <div className="selected-gallery-item">
+            {"Image " + selected + " is selected"}
+          </div>
+        }
+        <div className="image-gallery">
+          {images}
+        </div>
       </div>
     );
   }
-
-  return (
-    <div className="image-gallery">
-      {images}
-    </div>
-  );
 };
 
